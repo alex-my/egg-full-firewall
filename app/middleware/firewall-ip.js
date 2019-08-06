e034e8f1ebcde90012cf07e736a704f16128712d
alex <alex_my@126.com>
2019-08-06 11:46:12 +0800

    重写判断规则，判断所有的ip规则

--- app/middleware/firewall-ip.js
+++ app/middleware/firewall-ip.js
@@ -30,23 +30,23 @@ module.exports = (options, app) => {
   });
 
   // 预定义 JSON 转换结构
-  const stringify = fastJson({
-    title: 'Firewall IP Info',
-    type: 'object',
-    properties: {
-      pret_t: {
-        type: 'integer',
-      },
-      count: {
-        type: 'integer',
-      },
-      disable_t: {
-        type: 'integer',
-      },
-    },
-  }, {
-    uglify: true,
-  });
+  // const stringify = fastJson({
+  //   title: 'Firewall IP Info',
+  //   type: 'object',
+  //   properties: {
+  //     pret_t: {
+  //       type: 'integer',
+  //     },
+  //     count: {
+  //       type: 'integer',
+  //     },
+  //     disable_t: {
+  //       type: 'integer',
+  //     },
+  //   },
+  // }, {
+  //   uglify: true,
+  // });
 
   const checkIP = async (ip, method, path) => {
     // 被禁止
@@ -81,16 +81,15 @@ module.exports = (options, app) => {
       }
       return false;
     });
+
     if (ignoreRequestResult) {
-      logger.info(`path: ${path} in the ignore list`);
       return true;
     }
 
     const cacheKey = `${ipRedisPrefix}:${ip}`;
     const cacheValue = await redis.get(cacheKey);
-    const info = JSON.parse(cacheValue);
+    const rules = JSON.parse(cacheValue);
     /**
-     * info:
      * pre_t: 上一次检测的时间戳
      * count: 已访问的次数
      * disable_t: 封禁的时间戳
@@ -98,54 +97,68 @@ module.exports = (options, app) => {
     const t = _.parseInt(new Date() / 1000, 10);
 
     // 检测是否达到规则指定的条件
-    if (info) {
+    // 数量不相符表示更改了规则
+    if (rules && _.size(rules) === _.size(ipRule)) {
       // 是否在封禁时间内
-      if (info.disable_t > t) {
-        logger.info(`ip: ${ip} is currently in the limit period`);
-        return false;
-      }
-
-      info.count += 1;
-
-      const result = ipRule.some(rule => {
-        if (t - info.pret_t < rule.interval) {
-          if (info.count > rule.count) {
-            // 违反了本规则
-            redis.set(cacheKey, stringify({
-              pret_t: t,
-              count: 0,
-              disable_t: t + rule.expire,
-            }), 'EX', cacheExpire);
-            logger.info(`ip: ${ip} violation of rule: ${JSON.stringify(rule)}, t: ${t}, info: ${JSON.stringify(info)}`);
-            return true;
-          }
+      const isDisable = rules.some(rule => {
+        if (rule.disable_t > t) {
+          logger.info(`ip: ${ip} is currently in the limit period, rest time: ${t - rule.disable_t}s`);
+          return true;
         }
         return false;
       });
 
-      if (result) {
-        // 违法了规则
+      if (isDisable) {
         return false;
       }
-      // 没有违反规则，则记录本次访问
-      redis.set(cacheKey, stringify({
-        pret_t: info.pret_t,
-        count: info.count,
-        disable_t: 0,
-      }), 'EX', cacheExpire);
 
-      logger.debug(`update record, key: ${cacheKey}, pre_t: ${info.pret_t}, count: ${info.count}`);
-    } else {
-      // 没有任何记录，创建一个记录存储
-      redis.set(cacheKey, stringify({
+      // 检查是否违法了规则
+      let isLimit = false;
+      _.forEach(rules, (rule, index) => {
+        const definedRule = ipRule[index];
+        // logger.debug(`rule: ${JSON.stringify(rule)}, index: ${index}, definedRule: ${JSON.stringify(definedRule)}`);
+
+        rule.count += 1;
+        if (t - rule.pret_t < definedRule.interval) {
+          if (rule.count > definedRule.count) {
+            // 违反了规则
+            rule.disable_t = t + definedRule.expire;
+            isLimit = true;
+            logger.info(`ip: ${ip} violation of rule: ${JSON.stringify(rule)}, definedRule: ${JSON.stringify(definedRule)}, t: ${t}`);
+          }
+        } else {
+          // 重新计数
+          rule.pret_t = t;
+          rule.count = 0;
+          // logger.debug('reset rule');
+        }
+      });
+
+      // 更新本次数据
+      const value = JSON.stringify(rules);
+      redis.set(cacheKey, value, 'EX', cacheExpire);
+      // logger.debug(`update record, key: ${cacheKey}, value: ${value}`);
+
+      return !isLimit;
+    }
+
+    // 没有任何记录，创建一个记录存储
+
+    // 定义初始的缓存结构
+    const initRules = [];
+    _.forEach(ipRule, () => {
+      initRules.push({
         pret_t: t,
         count: 1,
         disable_t: 0,
-      }), 'EX', cacheExpire);
+      });
+    });
 
-      logger.debug(`new record, key: ${cacheKey}, pre_t: ${t}, count: 1`);
-    }
+    const value = JSON.stringify(initRules);
 
+    redis.set(cacheKey, value, 'EX', cacheExpire);
+
+    // logger.debug(`new record, key: ${cacheKey}, initRules: ${value}`);
     return true;
   };
 
